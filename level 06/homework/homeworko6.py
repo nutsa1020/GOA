866ea224658080ce916f45cd11282958a0fb7386
nutsa1020 <nutsamacharashvili68@gmail.com>
2024-06-01 15:58:39 +0400

    GOA

---
+++ level 06/homework/homeworko6.py
@@ -0,0 +1,16 @@
+name = "data"
+surname = "diasamidze"
+age = 100
+text = "I am programmer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
+
+
+name = "nutsa"
+surname = "macharashvili"
+age = 13
+text = "i am programer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
