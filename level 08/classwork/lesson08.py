54dec04f7df80c95bbf3f3a3013fe82d6137f775
nutsa1020 <nutsamacharashvili68@gmail.com>
2024-06-13 16:30:11 +0400

    goa

---
+++ level 08/classwork/lesson08.py
@@ -0,0 +1,11 @@
+#task 1
+#ricxvit dawyeba ar sheidzleba 
+#cvladis shemdeg davwerot texsti prchyalebis gareshe 
+#cvladis sheqmna ori sityvisgan matshoris speisit
+
+#task 2
+price = int(input("enter price"))
+discount = int(input("enter discount"))
+
+discounted_price = price - discount
+print(discounted_price)
---
+++ level 08/homework/homework02.py
@@ -0,0 +1,5 @@
+name = input("enter your name: ") # input არის ტერმინალში გამომტანი ფუნქცია
+last_name = input("enter your lastname: ")
+age = input("enter your age: ")
+
+print("my name is " + name + ", " + "my last name is " + last_name + ", " + "my age is " + age + ".") 
