217a2953777867728dabfe6a1d30a342b80e2a54
nutsa1020 <nutsamacharashvili68@gmail.com>
2024-07-16 21:32:58 +0400

    GOA

--- level 18/classwork/lesson.py
+++ level 18/classwork/lesson.py
@@ -1,86 +1,16 @@
-# for number in range(10):
-#     print (number)
+#8
+for i in reversed(range(0,101)):
+    print
 
-# 1)range ის გამოყენებით გამოიტანეთ ტერმინალში 30ჯერ hello world
-# 2)range ის გამოყენებით გამოიტანეთ ტერმინალში 1დან 10მდე რიცხვები
-# 3)range ის გამოყენებიტ გამოიტანეთ ტერმინლში 10დან 20 მდე 2 ის ჯერადი რიცხვები
+    #9
+    for i in reversed(range(100,0)):
 
-
-
-if word[i]. isupper():
-    Lists. append (i)
-return Lists
-
-print(capitals("ApPLe"))
-
-start, end , step
+        #4
+        for i in reversed(range(1000,0,100)):
 
 
+        #6 
+        while number !=100:
+            print("hello world")
 
 
-
-
-
-
-
-for number in range(0,10,5)
-print (number)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for i in range(30):
-print ("Hello World")
-for i in range (1,10) :
-print(i)
-for i in range(10,20,2):
-print (i)
-
-
-number = 20
-while number l= 20: # სანამ 5 არ უდრის 20
-print("Hello World")
-number = number + 1
-
-
-# for ჩვენ ვიცით გამეორების რაოდენობა
-# while დროს ჩვენ არ ვიცით გამეორებს რაოდენობა
-
-
-for i in range (1,10) :
-print（i）
-386
-387
-388
-389
-for i in range (10,20, 2) :
-print（2）
-390
-391
-number = 5
-392
-393
-394
-395
-while number 1= 20: # სანამ არ უდრის
-print("Hello World"2
-number = number +
-﻿
